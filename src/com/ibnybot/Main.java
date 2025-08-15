15022f06563e93b2a6fc35e0a9401f4a6f257550
IBanny <coding.ibanny@gmail.com>
2025-08-15 20:48:18 +0630

    fix some sleep threatt 1 minute

--- src/com/ibnybot/Main.java
+++ src/com/ibnybot/Main.java
@@ -6,14 +6,25 @@ import com.ibnybot.CodingAssistant; // Add this import
 
 public class Main {
     public static void main(String[] args) {
+
+        while (true) {
+            
+                System.out.println("Bot is still running...");
+
+
         try {
+             Thread.sleep(60000); //1minute
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
            botsApi.registerBot(new CodingAssistant("AIzaSyDjojLCjskhWGMD0YyQ0Vl8gB117fFOJdc"));
       //       botsApi.registerBot(new JavaAssistantBot("AIzaSyDjojLCjskhWGMD0YyQ0Vl8gB117fFOJdc"));
             System.out.println("Bot started successfully!");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+
     }
 };
 
